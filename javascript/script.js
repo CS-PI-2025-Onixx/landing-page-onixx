2e6906f3a3cc9cedc7bb2e8bd8883649b0ead185
danisfon <danieledasilvafonseca@gmail.com>
2025-04-08 22:12:36 -0300

    fix | desenvolvendo js do faq

--- javascript/script.js
+++ javascript/script.js
@@ -4,9 +4,21 @@ fetch('json/faq.json')
         const faqContainer = document.getElementById('faq-container');
 
         faqs.forEach(faq => {
+            const faqItem = document.createElement('div');
+            faqItem.classList.add('faq-item');
 
-            //DESENVOLVER AQUI
+            const pergunta = document.createElement('h4');
+            pergunta.textContent = faq.pergunta;
+            pergunta.addEventListener('click', () =>{
+                faqItem.classList.toggle('active');
+            })
 
+            const resposta = document.createElement('p');
+            resposta.textContent = faq.resposta;
+            
+            faqItem.appendChild(pergunta);
+            faqItem.appendChild(resposta);
+            faqContainer.appendChild(faqItem);
         });
     })
     .catch(e => {
